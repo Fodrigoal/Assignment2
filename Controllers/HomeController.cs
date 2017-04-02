9c6d9246eedbfb44e8efb2a71dd4af94d569b301
Rodrigo Silva <r.ribeiro88@gmail.com>
2017-04-02 09:19:14 -0700

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ZenithWebsite.Data;
+using Microsoft.EntityFrameworkCore;
+using ZenithWebsite.Models.ZenithSocietyModels;
+
+namespace ZenithWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string LONG_DATE_FORMAT = "MMMM dd, yyyy";
+        private ZenithContext db;
+
+        public HomeController(ZenithContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Index()
+        {
+            var @event = db.Event.Include(that => that.Activity);
+
+            Dictionary<String, List<Event>> Week = new Dictionary<String, List<Event>>();
+
+            //Find the monday of this week
+            DateTime today = DateTime.Now;
+            int deltaToMonday = DayOfWeek.Monday - today.DayOfWeek;
+            if (deltaToMonday > 0)
+                deltaToMonday -= 7; // Always get this weeks dates
+            DateTime monday = today.Date.AddDays(deltaToMonday);
+            DateTime nextMonday = monday.AddDays(7);
+            ViewBag.StartOfWeek = monday.ToString(LONG_DATE_FORMAT);
+
+            //Allow only days this week
+            var daysOfTheWeek = @event.Where(e => e.FromDate >= monday && e.FromDate < nextMonday);
+
+            //add to dictionary
+            foreach (var e in daysOfTheWeek.OrderBy(name => name.FromDate).ToList())
+            {
+                if (e.IsActive)
+                {
+                    if (Week.ContainsKey(e.FromDate.ToString(LONG_DATE_FORMAT)))
+                    {
+
+                        Week[e.FromDate.ToString(LONG_DATE_FORMAT)].Add(e);
+                    }
+                    else
+                    {
+                        Week[e.FromDate.ToString(LONG_DATE_FORMAT)] = new List<Event> { e };
+                    }
+                }
+            }
+
+            ViewBag.Week = Week.ToList();
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/ZenithSeeder.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZenithWebsite.Data;
+using ZenithWebsite.Models.ZenithSocietyModels;
+
+namespace ZenithWebsite.Data
+{
+    public class ZenithSeeder
+    {
+        public static void Seed(ZenithContext db)
+        {           
+            if (!db.Activity.Any())
+            {
+                db.Activity.AddRange(getActivities());
+                db.SaveChanges();   // Necessary for Event seeding
+            }
+            if (!db.Event.Any())
+            {
+                db.Event.AddRange(getEvents(db));
+                db.SaveChanges();
+            }
+        }
+
+        private static List<Activity> getActivities()
+        {
+            List<Activity> activities = new List<Activity>();
+            activities.Add(new Activity()
+            {
+                ActivityId = 1,
+                ActivityDescription = "Young ladies cooking lessons",
+                CreationDate = new DateTime(2015, 12, 30, 23, 59, 59)
+            });
+            activities.Add(new Activity()
+            {
+                ActivityId = 2,
+                ActivityDescription = "Youth choir practice",
+                CreationDate = new DateTime(2015, 12, 30, 23, 59, 59)
+            });
+            activities.Add(new Activity()
+            {
+                ActivityId = 3,
+                ActivityDescription = "Bingo Tournament",
+                CreationDate = new DateTime(2015, 12, 30, 23, 59, 59)
+            });
+            return activities;
+        }
+
+        private static List<Event> getEvents(ZenithContext db)
+        {
+            Random rand = new Random();
+            string[] randomNames = { "Amanda", "Bob", "Charles", "Dick", "Elaine", "Ford", "Zenith" };
+
+            // Base values 
+            var startDate = new DateTime(2017, 4, 1, 0, 0, 0);
+            var idCounter = 1;
+            var dayCounter = 1;
+
+            // Return value 
+            List<Event> events = new List<Event>();
+
+            for (int i = 0; i < 50; i++)
+            {
+                idCounter += 1;
+                dayCounter += rand.Next(4); // 0-3
+
+                var newStartDate = startDate.AddDays(dayCounter);
+                newStartDate = newStartDate.AddHours(rand.Next(23));
+                newStartDate = newStartDate.AddMinutes(rand.Next(60));
+
+                var newEndDate = new DateTime(
+                    newStartDate.Year, 
+                    newStartDate.Month, 
+                    newStartDate.Day, 
+                    newStartDate.Hour + 1, 
+                    newStartDate.Minute,
+                    newStartDate.Second
+                );
+
+                var randomName = randomNames[rand.Next(randomNames.Length)];
+
+                var randomActive = rand.Next(2) == 1 ? true : false;
+
+                var randomActivityId = rand.Next(1, 4);
+
+                var newEvent = new Event()
+                {
+                    EventId = idCounter,
+                    FromDate = newStartDate,
+                    ToDate = newEndDate,
+                    CreatedBy = randomName,
+                    IsActive = randomActive,
+                    CreationDate = newStartDate,
+                    Activity = db.Activity.First(a => a.ActivityId == randomActivityId)
+                };
+
+                events.Add(newEvent);
+            }
+
+            return events;
+        }
+    }
+}
